e3e175356e32a0baeafba6aed7b48a44a630e7e4
DanLevyM <dan.levy.pro@gmail.com>
2023-01-08 20:03:57 +0100

    templates

--- base/views.py
+++ base/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+rooms = [
+    {'id': 1, 'name': 'Python1'},
+    {'id': 2, 'name': 'Python2'},
+    {'id': 3, 'name': 'Python3'},
+]
 
-def room(request):
-    return render(request, 'home.html')
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
