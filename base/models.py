47f92565f105378178cd212801ed899ab074182e
DanLevyM <dan.levy.pro@gmail.com>
2023-01-10 23:00:59 +0100

    activity feed

--- base/models.py
+++ base/models.py
@@ -8,13 +8,13 @@ class Topic(models.Model):
     return self.name
 
 class Room(models.Model):
-  host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-  topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-  name = models.CharField(max_length=100)
-  description = models.TextField(null=True, blank=True)
-  participants = models.ManyToManyField(User, related_name='participants', blank=True)
-  updated = models.DateTimeField(auto_now=True)
-  created = models.DateTimeField(auto_now_add=True)
+  host          = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+  topic         = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+  name          = models.CharField(max_length=100)
+  description   = models.TextField(null=True, blank=True)
+  participants  = models.ManyToManyField(User, related_name='participants', blank=True)
+  updated       = models.DateTimeField(auto_now=True)
+  created       = models.DateTimeField(auto_now_add=True)
 
   class Meta:
     ordering = ['-updated', '-created']
@@ -24,11 +24,14 @@ class Room(models.Model):
     
 
 class Message(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  room = models.ForeignKey(Room, on_delete=models.CASCADE)
-  body = models.TextField()
+  user    = models.ForeignKey(User, on_delete=models.CASCADE)
+  room    = models.ForeignKey(Room, on_delete=models.CASCADE)
+  body    = models.TextField()
   updated = models.DateTimeField(auto_now=True)
   created = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ['-updated', '-created']
 
   def __str__(self):
     return self.body[0:50]
